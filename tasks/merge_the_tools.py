c13b3da0ad899246db112e639b9e0c5984093fa3
Դավիթ Գրիգորյան <69806082+grigoryan69@users.noreply.github.com>
2022-03-27 02:33:01 +0400

    Update merge_the_tools.py

--- tasks/merge_the_tools.py
+++ tasks/merge_the_tools.py
@@ -1,17 +1,9 @@
+from collections import OrderedDict
 def merge_the_tools(string, k):
-    split_string=(string[i:i+k] for i in range(0,len(string),k))
-    for i in split_string:
-        u=[]
-        u.append(i[0])
-        for j in range(1,len(i)):
-            if i[j] in u:
-                continue
-            else:
-                u.append(i[j])
+    for i in range(0, len(string), k):
+        print(''.join(OrderedDict.fromkeys(string[i : i + k])))
 
-        print(''.join(str(e) for e in u))
-
-
+        
 if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
