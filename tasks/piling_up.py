4e3a3e654d7a2a93d2d8ff7ca7a0203187f20af3
Դավիթ Գրիգորյան <69806082+grigoryan69@users.noreply.github.com>
2022-03-27 03:20:26 +0400

    Create piling_up.py

---
+++ tasks/piling_up.py
@@ -0,0 +1,16 @@
+def piling_up():
+    for _ in (range(int(input()))):
+        no_of_cubes = int(input())
+        side_lengths = list(map(int, input().strip().split()))
+        result = "Yes"
+        if max(side_lengths) not in (side_lengths[0], side_lengths[-1]):
+            result = "No"
+        print(result)
+
+
+def main():
+    piling_up()
+    
+    
+if __name__ == '__main__':
+    main()
